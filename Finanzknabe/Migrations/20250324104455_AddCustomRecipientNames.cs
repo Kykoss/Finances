198eabf389c837519e21fec2e04e3fda76099cd1
Robin Olde Meule <robin.olde.meule1997@gmail.com>
2025-03-24 12:42:58 +0100

    Add the possibility to set a custom display name for the recipients

---
+++ Finanzknabe/Migrations/20250324104455_AddCustomRecipientNames.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finanzknabe.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomRecipientNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomName",
+                table: "Recipients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomName",
+                table: "Recipients");
+        }
+    }
+}
